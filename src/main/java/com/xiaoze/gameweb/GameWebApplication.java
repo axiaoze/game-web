5699efc8e78a8dec9ee42cd1e4a31de77909c123
xiaoze <472385460@qq.com>
2022-02-22 17:32:55 +0800

    init gameWeb

---
+++ src/main/java/com/xiaoze/gameweb/GameWebApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoze.gameweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoze/gameweb/GameWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoze.gameweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
