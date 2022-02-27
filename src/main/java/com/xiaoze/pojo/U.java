549b769c7b16e1aae5347c2dc894f8e6edd317b7
xiaoze <472385460@qq.com>
2022-02-27 16:25:34 +0800

    login complete

--- src/main/java/com/xiaoze/pojo/U.java
+++
@@ -1,9 +0,0 @@
-package com.xiaoze.pojo;
-
-public class U {
-    public static void main(String[] args) {
-        String a = "abs23c";
-        String b = "abs23c";
-        System.out.println(!a.equals(b));
-    }
-}
